e3cd82ba2238244fd8b7ced828d0bfb3b5b75fc3
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-09-09 12:28:39 +0100

    update app

--- App.js
+++ App.js
@@ -1,41 +1,13 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Switch,
-  TouchableOpacity,
-  SafeAreaView,
-} from "react-native";
+import { Text, SafeAreaView } from "react-native";
+import { viewStyle, textStyle } from "./styles";
 
 export default function App() {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <SafeAreaView
-      style={{
-        height: "100%",
-        width: "100%",
-        paddingVertical: 20,
-        paddingHorizontal: 5,
-        backgroundColor: "#ccc",
-        alignItems: "center",
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 20,
-          color: "black",
-          width: "100%",
-          textAlign: "center",
-          paddingRight: 20,
-          paddingVertical: 5,
-        }}
-      >
-        Tobi Idowu
-      </Text>
+    <SafeAreaView style={viewStyle}>
+      <Text style={textStyle}>Tobi Idowu</Text>
     </SafeAreaView>
   );
 }
---
+++ styles.js
@@ -0,0 +1,19 @@
+export const viewStyle = {
+  height: "100%",
+  width: "100%",
+  paddingVertical: 20,
+  paddingHorizontal: 5,
+  backgroundColor: "#ccc",
+  alignItems: "center",
+  display: "flex",
+  justifyContent: "center",
+};
+
+export const textStyle = {
+  fontSize: 20,
+  color: "black",
+  width: "100%",
+  textAlign: "center",
+  paddingRight: 20,
+  paddingVertical: 5,
+};
