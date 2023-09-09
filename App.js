def115d155a57e9ec187d0e52e71590b3bade889
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-09-09 12:29:21 +0100

    update app

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { Text, SafeAreaView } from "react-native";
 import { viewStyle, textStyle } from "./styles";
 
 export default function App() {
-  const [darkTheme, setDarkTheme] = useState(false);
-
   return (
     <SafeAreaView style={viewStyle}>
       <Text style={textStyle}>Tobi Idowu</Text>
